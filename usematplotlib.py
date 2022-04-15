c16531f19238e96609b3b16e8e7e24e207cc0c39
Marios Magioladitis (Π2020239) <magioladitis@gmail.com>
2022-04-15 20:53:49 +0300

    Create usematplotlib.py
    Average through large files

---
+++ usematplotlib.py
@@ -0,0 +1,22 @@
+import os
+import matplotlib.pyplot as plt
+
+all_files = os.listdir()
+csv_files = []
+for f in all_files:
+	if '.csv' in f:
+		csv_files.append(f)
+for f_c in csv_files:
+	with open(f_c,'r') as file_csv:
+		raw_data = file_csv.readlines()
+	data = []
+	for l in raw_data:
+		s = ''
+		for ch in l:
+			if ch.isdigit():
+				s += ch
+		data.append(int(s))
+	print('File = ',f,'mean value = ',sum(data)/len(data))
+	plt.plot(data)
+	plt.savefig(f_c +'.png')
+	plt.show()
