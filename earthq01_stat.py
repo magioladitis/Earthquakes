5f1fe8e82d8d9bb4dbdfe11cc11880ad8763baea
Marios Magioladitis (Π2020239) <magioladitis@gmail.com>
2022-05-08 23:25:05 +0300

    Update earthq01_stat.py
    add 3 and 4

--- earthq01_stat.py
+++ earthq01_stat.py
@@ -18,3 +18,30 @@ def w_median(f,d,w):
 	plt.plot(m)
 	plt.title('Κυλιόμενη διάμεσος - παράθυρο δεδομένων = '+str(w))
 	plt.show()
+	
+def w_variance(f,d,w):
+	print('Κυλιόμενη διάμεσος αρχείου : ',f,' παράθυρο δεδομένων = ',w)
+	m = []
+	for i in range(len(d)-w):
+		m.append(statistics.pvariance(d[i:i+w]))
+	plt.plot(m)
+	plt.title('Κυλιόμενη διακύμανση - παράθυρο δεδομένων = '+str(w))
+	plt.show()
+	
+def w_min_max(f,d,w):
+	print('Κυλιόμενο εύρος αρχείου : ',f,' παράθυρο δεδομένων = ',w)
+	mi = []
+	ma = []
+	e = []
+	for i in range(len(d)-w):
+		mi.append(min(d[i:i+w]))
+		ma.append(max(d[i:i+w]))
+		e.append(ma[-1] - mi[-1])
+	plt.plot(mi,'b',label='minimum')
+	plt.plot(ma,'r',label='maximum')
+	plt.legend()
+	plt.title('Κυλιόμενα min max - παράθυρο δεδομένων = '+str(w))
+	plt.show()
+	plt.plot(e)
+	plt.title('Κυλιόμενο εύρος - παράθυρο δεδομένων = '+str(w))
+	plt.show()
